ec03098d2ceeb09b0a54ab6a25e11d25eb5a557a
Nico <nick.s.tsounis@gmail.com>
2018-07-14 17:06:06 +0200

    change port

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const publicPath = path.join(__dirname, '../public');
 const express = require('express');
-const port = process.env.PORT || 3000;
+const port = process.env.port || 3000;
 
 var app = express();
 

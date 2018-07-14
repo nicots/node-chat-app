02f822241a61e8d21a1569c45d7342019e2112a5
Nico <nick.s.tsounis@gmail.com>
2018-07-15 00:12:47 +0200

    New users, alert others

--- server/server.js
+++ server/server.js
@@ -15,9 +15,15 @@ io.on('connection', (socket) => {
     console.log('New user connected');
 
     socket.emit('newMessage', {
-        from: 'John',
-        text: 'See you then',
-        createdAt: 123123
+        from: 'Admin',
+        text: 'Welcome to the chat app',
+        createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+        from: 'Admin',
+        text: 'New user joined',
+        createdAt: new Date().getTime()
     });
 
     socket.on('createMessage', (message) => {
